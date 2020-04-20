5b770ac11e3f0efa0b3397d7d1484dc42a5e347e
jontymin <501211312@qq.com>
2020-04-20 10:10:09 +0800

    python框架1-GUI

---
+++ 框架/1、GUI/__init__.py
@@ -0,0 +1,16 @@
+# _*_ coding:UTf-8 _*_
+# 开发团队 ：jonty
+# 开发人员 ：jonty
+# 开发时间 ：2020/4/20 8:59
+# 文件名称 ：__init__.py.py
+# 开发工具 ：PyCharm
+
+"""
+GUI  Graphical User Interface 用户图形界面
+常用GUI框架
+wxPython
+Kivy
+Flexx
+PyQt
+Thinter
+"""
